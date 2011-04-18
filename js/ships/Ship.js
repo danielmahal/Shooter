17833037fc1f351208a80842e3fae574056ff0df
Daniel <daniel@unfold.no>
2011-04-18 12:01:12 +0200

    Add ships.

---
+++ js/ships/Ship.js
@@ -0,0 +1,44 @@
+var Ship = (function() {
+	function Ship(scene) {
+		var that = this;
+		
+		this.scene = scene;
+		
+		this.timer = 0;
+		this.obj = null;
+		this.momentum = 0;
+		this.angle = 0;
+		this.rotation = 0;
+		
+		this.shipColor = new THREE.Vector3(0, 1, 0);
+		
+		var material = new THREE.MeshShaderMaterial({
+			uniforms: {
+				shipColor: { type: "v3", value: this.shipColor }
+			},
+			vertexShader: document.getElementById('vertexShader').textContent,
+			fragmentShader: document.getElementById('fragment_shader2').textContent
+		});
+		
+		loader = new THREE.JSONLoader( true );
+		
+		loader.load({
+			model: "models/ship.js",
+			callback: function( geometry ) {
+				that.obj = THREE.SceneUtils.addMesh( scene, geometry, 20, 0, 0, 0, 0, 0, 0, material );
+				// that.shadow = new THREE.ShadowVolume( that.obj );
+				that.obj.position.y = 100;
+			}
+		});
+	}
+	
+	Ship.prototype.updateRotation = function() {
+		this.obj.rotation.y = this.rotation;
+	}
+	
+	Ship.prototype.destroy = function() {
+		this.scene.removeObject(this.obj);
+	}
+	
+	return Ship;
+})();
