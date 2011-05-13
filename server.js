cbad1e0d69bfc6e91c3f6cb0315b49ba396bb36e
Daniel <daniel@unfold.no>
2011-05-13 16:13:57 +0200

    changed sendhead to writehead in server

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var server = http.createServer(function (request, response) {
 		if(exists) {
 			fs.readFile(filename, "binary", function(err, file) {
 				if(!err) {
-					response.writeHead(200);
+					response.writeHead(200, {"Content-Type": "text/html"});
 					response.write(file, "binary");
-					response.ebd();
+					response.end();
 				}
 			});
 		}
