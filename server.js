3b180ce33dca0ebf03ee6b282ac51d9ac78772aa
Daniel <daniel@unfold.no>
2011-05-13 17:08:11 +0200

    server not working on nodester anymore

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http');
 var io = require('socket.io');
 
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (shooter) is running..');
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World\nApp (shooter) is running..');
 });
 
 server.listen(9984);
